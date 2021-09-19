bef9c67b8b3dc23ecc9d109b37c89b1bba4a820e
akospaska <82268269+akospaska@users.noreply.github.com>
2021-09-19 23:27:12 +0200

    in development

--- src/enteties/Customer.kt
+++
@@ -1,5 +0,0 @@
-package com.KtorTutorial.enteties
-
-data class Customer(val id: Int, val name: String)
-
-data class CustomerName(val name: String)
--- src/enteties/ToDo.kt
+++
@@ -1,3 +0,0 @@
-package com.KtorTutorial.enteties
-
-data class ToDo(val id: Int, var title: String, var done: Boolean)
